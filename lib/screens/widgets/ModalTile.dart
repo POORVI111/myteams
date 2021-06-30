a3365ed843b8e8cbb504726e1ef85ca111df3a69
POORVI111 <poorvigarg16@gmail.com>
2021-07-01 02:18:09 +0530

    made change in chat bottom layout

---
+++ lib/screens/widgets/ModalTile.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:myteams/utils/customtile.dart';
+
+class ModalTile extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final IconData icon;
+  final Function onTap;
+
+  const ModalTile({
+    @required this.title,
+    @required this.subtitle,
+    @required this.icon,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      child: CustomTile(
+        mini: false,
+        onTap: onTap,
+        leading: Container(
+          margin: EdgeInsets.only(right: 10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: Colors.white,
+          ),
+          padding: EdgeInsets.all(10),
+          child: Icon(
+            icon,
+            color: Colors.grey,
+            size: 38,
+          ),
+        ),
+        subtitle: Text(
+          subtitle,
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 14,
+          ),
+        ),
+        title: Text(
+          title,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+            fontSize: 18,
+          ),
+        ),
+      ),
+    );
+  }
+}
